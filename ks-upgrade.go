34e69b6973a4d19d0df1f9ad0ecf83efc02a8d7b
pixiake <guofeng@yunify.com>
2020-07-27 18:33:01 +0800

    Initial commit

---
+++ ks-upgrade.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/ghodss/yaml"
+	"github.com/kubesphere/kubekey/pkg/util"
+	"github.com/lithammer/dedent"
+	rbac "k8s.io/api/rbac/v1"
+	kubeErr "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"log"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+
+var (
+	globalrolebinding = template.Must(template.New("globalrolebinding").Parse(
+		dedent.Dedent(`apiVersion: iam.kubesphere.io/v1alpha2
+kind: GlobalRoleBinding
+metadata:
+  labels:
+    iam.kubesphere.io/user-ref: {{ .Name }}
+    kubefed.io/managed: "false"
+  name: {{ .Name }}-platform-admin
+roleRef:
+  apiGroup: iam.kubesphere.io
+  kind: GlobalRole
+  name: {{ .Role }}
+subjects:
+- apiGroup: rbac.authorization.k8s.io
+  kind: User
+  name: {{ .Name }}
+    `)))
+)
+
+func generateGlobalRoleBinding(name, role string) (string, error) {
+	return util.Render(globalrolebinding, util.Data{
+		"Name":   name,
+		"Role":   role,
+	})
+}
+
+func main() {
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	clusterrolebingdings, err := clientset.RbacV1().ClusterRoleBindings().List(context.TODO(), metav1.ListOptions{})
+
+	for _, clusterrolebingding := range clusterrolebingdings.Items {
+		if clusterrolebingding.RoleRef.Name == "cluster-admin" {
+			toGlobalRoleBinding(&clusterrolebingding, clientset, "platform-admin")
+		}
+		if clusterrolebingding.RoleRef.Name == "cluster-regular" {
+			toGlobalRoleBinding(&clusterrolebingding, clientset, "platform-regular")
+		}
+		if clusterrolebingding.RoleRef.Name == "workspaces-admin" {
+			toGlobalRoleBinding(&clusterrolebingding, clientset, "workspaces-admin")
+		}
+	}
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
+
+func toGlobalRoleBinding(binding *rbac.ClusterRoleBinding, clientset *kubernetes.Clientset, role string)  {
+	for _, subject := range binding.Subjects {
+		if subject.APIGroup == "rbac.authorization.k8s.io" && subject.Kind == "User" {
+			name := subject.Name
+			objStr, err := generateGlobalRoleBinding(name, role)
+			if err != nil {
+				log.Fatal(err)
+			}
+			j2, err1 := yaml.YAMLToJSON([]byte(objStr))
+			if err1 != nil {
+				log.Fatal(err)
+			}
+			err2 := clientset.RESTClient().Post().
+				AbsPath("/apis/iam.kubesphere.io/v1alpha2/globalrolebindings").
+				Body(j2).
+				Do(context.TODO()).Error()
+			if err2 != nil {
+				if kubeErr.IsAlreadyExists(err2) {
+					log.Printf("%s is already exists.\n", name)
+				} else {
+					log.Fatal(err2)
+				}
+			}
+			if err := clientset.RbacV1().ClusterRoleBindings().Delete(context.TODO(), binding.Name, metav1.DeleteOptions{}); err != nil{
+				log.Fatal(err)
+			}
+		}
+	}
+}
+
